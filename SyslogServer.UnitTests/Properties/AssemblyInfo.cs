e516e89df1f6057d10b8e2201daa6d56b3b1efb9
Pavel Marinov <pavel.g.marinov@gmail.com>
2017-12-13 12:07:40 +0200

    test project folder name fix

--- SyslogServer.UnitTests/Properties/AssemblyInfo.cs
+++ SyslogServer.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SyslogServer.Tests")]
+[assembly: AssemblyTitle("SyslogServer.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SyslogServer.Tests")]
+[assembly: AssemblyProduct("SyslogServer.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
